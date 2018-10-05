e3a9757a99d0cea34c3b7621003f3d44dd2aa761
RyuTribal <ivan.sedelkin9@gmail.com>
2018-10-05 16:23:34 +0200

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+
+        public ViewResult Index2()
+        {
+            return View();
+        }
+        //"default"
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RSVP()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RSVP(orderResponse gr)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("BetterBe", gr);
+            }
+            else
+            {
+                return View();
+
+            }
+            
+        }
+    }
+}
---
+++ WebApplication1/Models/orderResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.Models
+{
+    public class orderResponse
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Pizza { get; set; }
+        [Required]
+        public string Drink { get; set; }
+        [Required]
+        public string Snack { get; set; }
+    }
+}
